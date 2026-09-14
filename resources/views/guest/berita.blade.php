d2c3e8a55a69c18d2713af8d40d1cfc94efff084
faridadilahz <mfaridadilahh@gmail.com>
2026-09-14 11:32:44 +0700

    create berita and route berita in web php

---
+++ resources/views/guest/berita.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en" data-bs-theme="light">
+    <head>
+        <title>Title</title>
+        <!-- Required meta tags -->
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+        <!-- Bootstrap CSS v5.3.8 -->
+        <link
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css"
+            rel="stylesheet"
+            integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB"
+            crossorigin="anonymous"
+        />
+    </head>
+
+    <body>
+        <header>
+            @include('partials.guest.navbar')
+        </header>
+        <main class="mt-5 pt-6" style="background-color: #f5f5f5;">
+            <div class="d-flex text-center flex-wrap justify-content-center align-items-center px-5 py-5">
+            <div class="container">
+            <h2 class="fw-bold mb-3">Berita Seruli</h2>
+            <div class="row g-4 justify-content-center">
+                @include('partials.guest.cardberita')
+            </div>
+            </div>
+        </div>
+        </main>
+        <footer>
+            @include('partials.guest.footer')
+        </footer>
+        <!-- Bootstrap JavaScript Bundle (includes Popper) -->
+        <script
+            src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
+            integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI"
+            crossorigin="anonymous"
+        ></script>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/beranda', function () {
     return view('guest.beranda');
 });
+
+Route::get('/berita', function () {
+    return view('guest.berita');
+});
+
