7af432004cdbe1f96a0670d0d0d60b0d179184b8
faridadilahz <mfaridadilahh@gmail.com>
2026-09-14 09:42:20 +0700

    fill model beritas and galeris

--- app/Models/Beritas.php
+++ app/Models/Beritas.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Beritas extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'beritas';
+
+    protected $fillable = [
+        'gambarberita',
+        'judulberita',
+        'deskripsiberita',
+    ];
 }
--- app/Models/Galeris.php
+++ app/Models/Galeris.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Galeris extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'galeris';
+
+    protected $fillable = [
+        'gambargaleri',
+        'judulgaleri',
+    ];
 }
