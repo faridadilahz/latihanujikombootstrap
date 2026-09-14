8e19edda012dcd17f01924a373eb8ac9ce91e022
faridadilahz <mfaridadilahh@gmail.com>
2026-09-14 20:35:47 +0700

    create partials admin statistic

---
+++ resources/views/partials/admin/statistic.blade.php
@@ -0,0 +1,18 @@
+<div class="row g-4">
+    <div class="col-md-0 col-sm-6">
+        <div class="card card-light h-100 border-0">
+            <div class="card-body">
+                <h1 class="card-title text-primary fw-bold">4</h1>
+                <p class="text-secondary">Jumlah Posting Berita</p>
+            </div>
+        </div>
+    </div>
+    <div class="col-md-0 col-sm-6">
+        <div class="card card-light h-100 border-0">
+            <div class="card-body">
+                <h1 class="card-title text-primary fw-bold">3</h1>
+                <p class="text-secondary">Jumlah Posting Galeri</p>
+            </div>
+        </div>
+    </div>
+</div>
