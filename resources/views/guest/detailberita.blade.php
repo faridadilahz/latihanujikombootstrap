a33312096edb6d1016b3a0b32fe3221d92181f6c
faridadilahz <mfaridadilahh@gmail.com>
2026-09-16 08:36:53 +0700

    add created date in berita and galeri

--- resources/views/guest/detailberita.blade.php
+++ resources/views/guest/detailberita.blade.php
@@ -7,9 +7,12 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
 
+
     <!-- Bootstrap CSS v5.3.8 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 
 <body>
@@ -18,7 +21,8 @@
     </header>
     <main class="mt-5 pt-6" style="background-color: #f5f5f5;">
         <div class="d-flex text-center flex-column justify-content-center align-items-center px-5 py-5">
-            <h2 class="fw-bold mb-3">{{ $beritas->judulberita }}</h2>
+            <p class="text-secondary mb-4"><i class="fa-regular fa-calendar me-2"></i>{{ $beritas->created_at->locale('id')->translatedFormat('d F Y') }}</p>
+            <h2 class="fw-bold mb-4">{{ $beritas->judulberita }}</h2>
             <img src="{{ asset('storage/' . $beritas->gambarberita) }}" alt="" class="rounded-3 mb-3"
                 style="max-width: 850px;">
             <p class="text-secondary" style="max-width: 850px;">{{ $beritas->deskripsiberita }}</p>
--- resources/views/guest/detailgaleri.blade.php
+++ resources/views/guest/detailgaleri.blade.php
@@ -10,6 +10,8 @@
     <!-- Bootstrap CSS v5.3.8 -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous" />
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
 </head>
 
 <body>
@@ -18,6 +20,7 @@
     </header>
     <main class="mt-5 pt-6" style="background-color: #f5f5f5;">
         <div class="d-flex text-center flex-column justify-content-center align-items-center px-5 py-5">
+            <p class="text-secondary"><i class="fa-regular fa-calendar me-2"></i>{{ $galeris->created_at->locale('id')->translatedFormat('d F Y')}}</p>
             <h2 class="fw-bold mb-4">{{ $galeris->judulgaleri }}</h2>
             <img src="{{ asset('storage/' . $galeris->gambargaleri) }}" alt="" class="rounded-3 mb-3"
                 style="max-width: 850px;">
