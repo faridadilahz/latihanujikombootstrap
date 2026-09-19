e1936605889d4b84e0462c2f4bd4a8a77f804039
faridadilahz <mfaridadilahh@gmail.com>
2026-09-19 10:27:46 +0700

    fixing navbar in desktop

--- resources/views/partials/guest/navbar.blade.php
+++ resources/views/partials/guest/navbar.blade.php
@@ -1,5 +1,5 @@
 <div class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top z-3 p-3 px-5">
-    <div class="container-fluid">
+    <div class="container-fluid position-relative">
         <a href="/beranda" class="d-flex align-items-center text-decoration-none">
             <img src="../assets/img/logoseyciswhite.png" class="me-1" style="max-width: 36px; object-fit: cover;">
             <p class="navbar-brand fw-bold mb-0">Seycis</p>
@@ -10,24 +10,24 @@
             <span class="navbar-toggler-icon"></span>
         </button>
 
-        <div class="collapse navbar-collapse position-lg-absolute start-lg-50 translate-middle-lg-x" id="navbarNav">
-            <ul class="navbar-nav">
-                <li class="nav-item-active me-4">
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mx-auto text-center">
+                <li class="nav-item-active me-lg-4">
                     <a href="/beranda"
                         class="nav-link {{ request()->is('beranda') || request()->is('/') ? 'active' : '' }}">Beranda</a>
                 </li>
-                <li class="nav-item-active me-4">
+                <li class="nav-item-active me-lg-4">
                     <a href="/berita"
                         class="nav-link {{ request()->is('berita') || request()->is('/') ? 'active' : '' }}">Berita</a>
                 </li>
-                <li class="nav-item-active me-4">
+                <li class="nav-item-active me-lg-4">
                     <a href="/galeri"
                         class="nav-link {{ request()->is('galeri') || request()->is('/') ? 'active' : '' }}">Galeri</a>
                 </li>
             </ul>
 
 
-            <div class="ms-auto">
+            <div class="text-center">
                 <a href="/login" class="btn btn-light fw-semibold text-primary">Masuk</a>
             </div>
         </div>
